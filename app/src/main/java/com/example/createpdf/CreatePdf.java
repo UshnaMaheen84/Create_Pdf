578d2b8ed8207c499516c0bbf0055abb3bbd7f09
UshnaMaheen84 <ushnamaheen48@gmail.com>
2021-05-03 11:39:48 +0500

    initial commit 3/5/21

--- app/src/main/java/com/example/createpdf/CreatePdf.java
+++ app/src/main/java/com/example/createpdf/CreatePdf.java
@@ -8,12 +8,14 @@ import android.os.Environment;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.itextpdf.layout.element.Paragraph;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 
-
+import com.itextpdf.layout.element.Cell;
+import com.itextpdf.layout.element.Table;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -48,31 +50,22 @@ public class CreatePdf extends AppCompatActivity {
             PdfWriter writer = PdfWriter.getInstance(document,outputStream);
             document.open();
 
-            PdfPTable table = new PdfPTable();
             float [] pointColumnWidths = {150F, 150F, 150F};
             Table table = new Table(pointColumnWidths);
 
             // Adding cells to the table
-            table.addCell(new Cell().add("Name"));
-            table.addCell(new Cell().add("Rimsha"));
-            table.addCell(new Cell().add("Id"));
-            table.addCell(new Cell().add("1001"));
-            table.addCell(new Cell().add("Designation"));
-            table.addCell(new Cell().add("Programmer"));
+            table.addCell(new Cell().add(new Paragraph("Name")));
+            table.addCell(new Cell().add(new Paragraph("Rimsha")));
+            table.addCell(new Cell().add(new Paragraph("Id")));
+            table.addCell(new Cell().add(new Paragraph("1001")));
+            table.addCell(new Cell().add(new Paragraph("Designation")));
+            table.addCell(new Cell().add(new Paragraph("Programmer")));
+
 
         } catch (DocumentException e) {
             e.printStackTrace();
     }
 
-//    private void createPdfa() throws FileNotFoundException {
-//
-//        String path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).toString() ;
-//        File file = new File(path,"myPdf.pdf");
-//        OutputStream outputStream = new FileOutputStream(file);
-//
-//        PdfWriter writer = new PdfWriter(file);
-//        PdfDocument pdfDocument = new PdfDocument(writer);
-//        Document document = new Document(pdfDocument);
-//    }
+
 }
 }
